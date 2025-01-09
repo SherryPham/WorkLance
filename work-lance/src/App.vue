2edc2663f12ff521b1a9ed9f597579c46ab16b37
Sherry <phamthuthu204@gmail.com>
2025-01-09 17:12:03 +1100

    HomeCard and Card container components

--- work-lance/src/App.vue
+++ work-lance/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
 import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
 </script>
 <template>
   <NavBar />
   <Hero />
+  <HomeCards />
 </template>
