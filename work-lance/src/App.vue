5a46a5f7878ed890e121ac7106636ac9a819e3cd
Sherry <phamthuthu204@gmail.com>
2025-01-09 17:39:40 +1100

    Jobs2.json

--- work-lance/src/App.vue
+++ work-lance/src/App.vue
@@ -2,9 +2,11 @@
 import NavBar from "@/components/NavBar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
 <template>
   <NavBar />
   <Hero />
   <HomeCards />
+  <JobListings />
 </template>
